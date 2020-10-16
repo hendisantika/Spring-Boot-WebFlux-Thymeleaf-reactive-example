82de28679776ce92020decb95105346f1a6cf0eb
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-17 05:51:59 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Spring-Boot-WebFlux-Thymeleaf-reactive-example
---
+++ src/main/java/com/hendisantika/webfluxthymeleafreactive/WebfluxThymeleafReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.webfluxthymeleafreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxThymeleafReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxThymeleafReactiveApplication.class, args);
+    }
+
+}
